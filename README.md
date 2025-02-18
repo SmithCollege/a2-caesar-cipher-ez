7f929656976b5feabc350c529001515823902711
ezdancewic <ezdancewic@smith.edu>
2025-02-17 21:20:21 -0500

    finished assignment

--- README.md
+++ README.md
@@ -4,17 +4,23 @@
 
 Your readme should include the following information. Each student needs to submit all of this information independently, even when pair programming. 
 
-Programming Partner Name:
+Programming Partner Name: N/A
 
-Other Collaborators (and kudos to people who were especially helpful):
+Other Collaborators (and kudos to people who were especially helpful): 
+My father, Steve Zdancewic helped me set up gradle. Frankie McRedmond helped me talk through some logic errors. 
 
-References Used:
+References Used: 
+Java Documentation
+https://www.geeksforgeeks.org/how-to-initialize-an-array-in-java/ 
 
 
 ## Reflection Questions
 
 What was your favorite part of this assignment?
+I liked how satisfying it was to run a test and have it work. It was also kind of fun to write in secret codes. 
 
 What was most challenging about this assignment?
+I was confused as to how gradle worked (I thought the first step was getting the program to build, but you need everything to run without exceptions and pass all the tests first before you could do that so I was confused.) Also I had a hard time on the decode method because it was confusing to think backwards and inverse everything I did for encode. 
 
 General comments on your experience with this assignment:
+I feel like I understand the reason for stubs and unit testing a lot better now. I feel like there could've been a more efficient way to do it rather than making an array and then turning the array into a dynamic array but that's what I did. 
--- app/src/main/java/a1template/DynamicArray.java
+++ app/src/main/java/a1template/DynamicArray.java
@@ -5,9 +5,52 @@ package a1template;
 public class DynamicArray<T> implements IndexAccess<T>{
     
     // What instance variables do you need?
+    int arrayLength;
+    T[] array; 
 
     // Write a constructor to make a new DynamicArray Object from an array
+    public DynamicArray(int arrayLength, T[] array){
+        this.arrayLength = arrayLength;
+        this.array = array;
+    }
 
     // Now implement the methods from IndexAccess?
+    
+    /** Returns the value stored at a given index
+     * @param i index of element to read
+     * @return value stored at the given index
+     */
+    public T get(int i){
+        return this.array[i];
+    }
 
+    /** Returns the value stored at a given index plus the offset
+     * @param i index of element to read
+     * @param offset adjust index by this value
+     * @return value stored at the given index
+     */
+    public T get(int i, int offset){
+        return this.array[i + offset];
+    }
+
+    /** Stores the given value at the given index
+     * @param i index of locaßtion to store
+     * @param val value to store at given index
+     */
+    public void set(int i, T val){
+        this.array[i] = val; 
+    }
+
+    /**
+     * Converts the DynamicArray to a String 
+     * @return the contents of the DynamicArray as strings concatenated by commas
+     */
+    public String toString(){
+        String output = ""; 
+        for(int i = 0; i < this.array.length; i++){
+            String x = this.array[i].toString();
+            output = output + x;
+        }
+        return output;
+    }
 }
